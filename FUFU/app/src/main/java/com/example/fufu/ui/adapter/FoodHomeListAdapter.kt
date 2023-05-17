36b43ca4321733fbe27cd06918b4fe7551dc0085
Nguyen Truong Chi <nguyentruongchi83@gmail.com>
2023-05-17 07:27:57 +0700

    chidaika changed

--- FUFU/app/src/main/java/com/example/fufu/ui/adapter/FoodHomeListAdapter.kt
+++ FUFU/app/src/main/java/com/example/fufu/ui/adapter/FoodHomeListAdapter.kt
@@ -19,9 +19,13 @@ class FoodHomeListAdapter(var foodHomeList: List<HomeFood>, var itemListener: Cl
     : RecyclerView.Adapter<FoodHomeListAdapter.FoodHomeListViewHolder>(){
 
     class FoodHomeListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val foodId: TextView = view.findViewById(R.id.food_id)
         val foodImg: ImageView = view.findViewById(R.id.img_food_around_you)
         val foodName: TextView = view.findViewById(R.id.item_name_around_you)
         val foodPrice: TextView = view.findViewById(R.id.item_price_around_you)
+        val foodResId: TextView = view.findViewById(R.id.res_id_round)
+        val foodDes: TextView = view.findViewById(R.id.item_des)
+        val foodResName: TextView = view.findViewById(R.id.item_resName)
         val itemClick: MaterialCardView = view.findViewById(R.id.layoutClick2)
     }
 
@@ -32,10 +36,13 @@ class FoodHomeListAdapter(var foodHomeList: List<HomeFood>, var itemListener: Cl
 
     override fun onBindViewHolder(holder: FoodHomeListViewHolder, position: Int) {
         val homeFood: HomeFood = foodHomeList[position]
+        holder.foodId.text = homeFood.itemId.toString()
+        holder.foodResId.text = homeFood.resId.toString()
         Glide.with(holder.foodName.context).load("http://192.168.1.131:80/fufuAPI/images/" + homeFood.itemImg).into(holder.foodImg)
         holder.foodName.text = homeFood.itemName
         holder.foodPrice.text = homeFood.itemPrice.toString()
-
+        holder.foodDes.text = homeFood.itemDes
+        holder.foodResName.text = homeFood.resName
         holder.itemClick.setOnClickListener {
             itemListener.onClickItemFoodHome(homeFood)
         }
