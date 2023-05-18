07e6191ea0707e817f68e3261a7e701fd392dc71
Nguyen Truong Chi <nguyentruongchi83@gmail.com>
2023-05-19 01:26:23 +0700

    chi changed

--- FUFU_SERVER_PHP/public/signIn.php
+++ FUFU_SERVER_PHP/public/signIn.php
@@ -1,7 +1,7 @@
 <?php
-if (!empty($_POST["phone"]) && !empty($_POST["pass"])) {
-    $phone = $_POST["phone"]; // post email
-    $pass = $_POST["pass"]; // post password
+//if (!empty($_POST["phone"]) && !empty($_POST["pass"])) {
+    $phone = "0969634430";//$_POST["phone"]; // post email
+    $pass = "123456789";//$_POST["pass"]; // post password
     $rs = array();
     $con = mysqli_connect("localhost", "root", "", "fufu_db");
     if ($con) {
@@ -12,20 +12,43 @@ if (!empty($_POST["phone"]) && !empty($_POST["pass"])) {
             if ($phone == $row['userPhone'] && password_verify($pass, $row['userPass'])) {
                 $sqlUpdate = "UPDATE user SET userStatus = 1 WHERE userPhone = '$phone'";
                 if (mysqli_query($con, $sqlUpdate)) {
-                    $rs = array("status" => "success", 
-                                "email" => $row['userEmail'], 
-                                "phone" => $row['userPhone'],
-                                "name" => $row['userFullName'],
-                                "address" => $row['userAddress'],
-                                "gender" => $row['userGender'],
-                                "dob" => $row['userDob'],
-                                "bio" => $row['userBio'],
-                                "userStatus" => $row['userStatus']);
+                    if ($row['userRole'] == 1) {
+                        $getId = $row['userId'];
+                        $sqlHaveRes = "SELECT * FROM user INNER JOIN restaurant ON user.userId = restaurant.userId WHERE user.userId = '$getId'";
+                        $result = mysqli_query($con, $sqlHaveRes);
+                        if (mysqli_num_rows($result) != 0) {
+                            $rowUser = mysqli_fetch_assoc($result);
+                            $rs = array("status" => "success",
+                                        "userId" => $rowUser['userId'],
+                                        "resId" => $rowUser['resId'],
+                                        "email" => $rowUser['userEmail'], 
+                                        "phone" => $rowUser['userPhone'],
+                                        "name" => $rowUser['userFullName'],
+                                        "address" => $rowUser['userAddress'],
+                                        "gender" => $rowUser['userGender'],
+                                        "dob" => $rowUser['userDob'],
+                                        "bio" => $rowUser['userBio'],
+                                        "role" => $rowUser['userRole'],
+                                        "userStatus" => $rowUser['userStatus']);
+                        }
+                    } else {
+                        $rs = array("status" => "success",
+                                    "userId" => $row['userId'],
+                                    "email" => $row['userEmail'], 
+                                    "phone" => $row['userPhone'],
+                                    "name" => $row['userFullName'],
+                                    "address" => $row['userAddress'],
+                                    "gender" => $row['userGender'],
+                                    "dob" => $row['userDob'],
+                                    "bio" => $row['userBio'],
+                                    "role" => $row['userRole'],
+                                    "userStatus" => $row['userStatus']);
+                    }
                 } else $rs = array("status" => "failed", "message" => "ko");
             } else $rs = array("status" => "failed", "message" => "Email or Password incorrect");
         } else $rs = array("status" => "failed", "message" => "Email or Password incorrect");
     } else $rs = array("status" => "failed", "message" => "Couldn't connect to server");
-} else $rs = array("status" => "failed", "message" => "All fields are required");
+//} else $rs = array("status" => "failed", "message" => "All fields are required");
 
 echo json_encode($rs, JSON_PRETTY_PRINT);
 
